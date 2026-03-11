d5f08cfd67cdffa980e03e8f69ad40db1e6db79e
MK <145795329+MK04-NextStep@users.noreply.github.com>
2026-03-11 23:42:59 +0530

    Create Home.jsx

---
+++ Mini Projects/Star Rating/Home.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import { FaRegStar } from "react-icons/fa6";
+import { FaStar } from "react-icons/fa6";
+import './App.css'
+function Home() {
+    let total = 5;
+    let [selected, setSelected] = useState(0);
+    return(
+        <div className="body">
+        <h1>Star Rating</h1>
+        <div className="stars">
+            {
+                Array.from({ length: total}, (_,i) => {
+                    const value = i + 1
+                    return value <= selected ?
+                        <FaStar key={value} onClick={() => setSelected(value)}/> :
+                        <FaRegStar key={value} onClick={() => setSelected(value)}/>
+                })
+            }
+        </div>
+        { selected > 0 && <p>Rating: {selected}</p> }
+        <button onClick={() => setSelected()}>Clear Rating</button>
+    </div>
+    )
+}
+export default Home
